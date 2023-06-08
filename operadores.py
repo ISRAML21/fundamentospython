6ebbf15ab5955b3be8a7ff106d0c35bbc3e00bba
Israel <jaime.munozaba@gmail.com>
2023-06-08 14:43:08 -0600

    03 Operadores

---
+++ operadores.py
@@ -0,0 +1,18 @@
+# Operadores Matematicos
+# + - * %
+# / division flotante
+# // divison entera
+# ** potencia
+print(24 + 62)
+print(50 - 32)
+print(35 * 2)
+print(10 % 3)
+print(10 / 3)
+print(10 // 3)
+print(10 ** 3)
+
+# OPERADORES String
+# + para contatenar
+# * replicar un string
+print("Hola a " + "todos!")
+print("DSM 3A" * 4)
