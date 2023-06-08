cf96adab1a43daae3388944def547e2a65a6b8f7
Israel <jaime.munozaba@gmail.com>
2023-06-08 14:27:11 -0600

    Iniciando con python

---
+++ main.py
@@ -0,0 +1,10 @@
+print("ESTE ES UN STRING","Hola Mundo!")
+print("ESTE ES UN ENTERO", 35)
+print("ËSTE ES UN FLOAT", 3.14)
+print("ESTE ES UN BOOLEANO", True)
+
+#function  type nos permite saber tipos de dato
+print(type(True))
+print(type(3.14))
+print(type(3))
+print(type(""))
---
+++ variables.py
@@ -0,0 +1,13 @@
+#python es NO TIPADO
+name = "Jaime"
+age = 19
+is_single = True
+print(name)
+print(age)
+print(is_single)
+
+"""
+HOLA SOY JAIME, TENGO 19 AÑOS Y SOY SOLTERO = True
+"""
+#Utilizando format String
+print("Hola soy {}, tengo {} años y soltero = {}".format(name, age, is_single))
