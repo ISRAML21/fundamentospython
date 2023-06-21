f43c60757c204cc14b0dee0d31aa0750379bce7c
Israel <jaime.munozaba@gmail.com>
2023-06-21 14:41:10 -0600

    10 operadores logicos y booleanos

---
+++ operadores2.py
@@ -0,0 +1,42 @@
+#Operadores matematicos
+#+-*%
+#/Division flotante
+#//Division Entera
+#**Potencia
+print(10+2)
+print(10-2)
+print(10*2)
+print(10%2)
+print(10/2)
+print(10//2)
+print(10**2)
+
+#operadores booleanos
+print("Operadores booleanos")
+#menor que, menor o igual que
+print(10<10)
+print(10<=10)
+#mayor que, mayor o igual que
+print(10>10)
+print(10>=10)
+#igual a
+print(10==10)
+#diferente de
+print(10!=10)
+
+#operadores logicos
+print("Operadores logicos")
+#and
+print(True and True)
+print(True and False)
+#or
+print(False or False)
+print(False and True)
+#not
+print(not True)
+print(not False)
+#Operadores string
+# + para contatenar
+# * para replicar
+print("Hola mundo" + ". Es mi primer codigo")
+print(" Hola mundo " * 3)
